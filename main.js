4f908fae3aef375a53c621ea23d6fba187ae22c2
dev-jeong <need9306@gmail.com>
2022-03-26 15:12:39 +0900

    first commit

--- main.js
+++ main.js
@@ -1,24 +1,25 @@
 require('dotenv').config();
 const puppeteer = require('puppeteer');
 
-let browser, page1, page2, page3, page4, page5
-let promises = [];
+let browser, page1, page2
 
 (async () => {
     browser = await puppeteer.launch({ headless: process.env.BROWSER === '0' ? false : true })
 
+    page1 = await browser.newPage()
     await login()
 
-    await Promise.all([
-        soldout(),
-        reinventory(),
-        update(),
-        send()
-    ])
+    page2 = await browser.newPage()
+    await page2.setDefaultNavigationTimeout(0);
+    while (true) {
+        await soldout()
+        await reinventory()
+        await update()
+        await send()
+    }
 })();
 
 async function login() {
-    page1 = await browser.newPage();
     await page1.goto('https://domeggook.com/ssl/member/mem_loginForm.php')
     await page1.type('#idInput', process.env.ID)
     await page1.type('#formLogin > input:nth-child(6)', process.env.PW)
@@ -28,43 +29,27 @@ async function login() {
 }
 
 async function soldout() {
-    page2 = await browser.newPage();
     await page2.goto('https://domemedb.domeggook.com/index/popup_sender/popup_soldOut.php?b2bStatus=0&sstore=0')
     await page2.evaluate(() => {
-        sendSoldout();
+        sendSoldout()
     });
-    await page2.close();
-    await new Promise(resolve => setTimeout(resolve, parseInt(process.env.INTERVAL1) * 1000));
-    soldout()
+    await new Promise(resolve => setTimeout(resolve, parseInt(process.env.INTERVAL) * 1000));
 }
 
 async function reinventory() {
-    page3 = await browser.newPage();
-    await page3.goto('https://domemedb.domeggook.com/index/popup_sender/popup_reInventory.php?b2bStatus=0&sstore=0')
-    await page3.evaluate(() => {
-        sendReinventory();
+    await page2.goto('https://domemedb.domeggook.com/index/popup_sender/popup_reInventory.php?b2bStatus=0&sstore=0')
+    await page2.evaluate(() => {
+        sendReinventory()
     });
-    await page3.close();
-    await new Promise(resolve => setTimeout(resolve, parseInt(process.env.INTERVAL1) * 1000));
-    reinventory()
+    await new Promise(resolve => setTimeout(resolve, parseInt(process.env.INTERVAL) * 1000));
 }
 
 async function update() {
-    page4 = await browser.newPage();
-    await page4.goto('https://domemedb.domeggook.com/index/popup_sender/popup_updateProduct.php?arr=all&b2bStatus=0&status=PRICE&fromOversea=0&sf=&sw=&itemNos=&pageLimit=50&b2bStatus=0&template=&sender_date1=2022-01-27&sender_date2=2022-02-27&dateType=3&template=&sstore=0&update=ok&&nmChkVal=1')
-    await page4.close();
-    await new Promise(resolve => setTimeout(resolve, parseInt(process.env.INTERVAL1) * 1000));
-    update()
+    await page2.goto('https://domemedb.domeggook.com/index/popup_sender/popup_updateProduct.php?arr=all&b2bStatus=0&status=PRICE&fromOversea=0&sf=&sw=&itemNos=&pageLimit=50&b2bStatus=0&template=&sender_date1=&sender_date2=&dateType=3&template=&sstore=0&update=ok&&nmChkVal=1')
+    await new Promise(resolve => setTimeout(resolve, parseInt(process.env.INTERVAL) * 1000));
 }
 
 async function send() {
-    page5 = await browser.newPage();
-    page5.on('dialog', async dialog => {
-        await dialog.accept();
-    });
-    await page5.goto('https://domemedb.domeggook.com/index/sender/sender_productList.php?sstore=0&fromOversea=0&dateType=3&sender_date1=2022-01-27&sender_date2=2022-02-27&status=SUCCESS')
-    await page5.click('body > div:nth-child(3) > div:nth-child(6) > table > tbody > tr > td > div:nth-child(2) > a')
-    await page5.close();
-    await new Promise(resolve => setTimeout(resolve, parseInt(process.env.INTERVAL2) * 1000));
-    send()
+    await page2.goto('https://domemedb.domeggook.com/index/popup_sender/popup_updateProduct.php?arr=all&b2bStatus=0&status=SUCCESS&fromOversea=0&sf=&sw=&itemNos=&pageLimit=50&b2bStatus=0&template=&sender_date1=&sender_date2=&dateType=7&template=&sstore=0&update=ok&&nmChkVal=0')
+    await new Promise(resolve => setTimeout(resolve, parseInt(process.env.INTERVAL) * 1000));
 }
