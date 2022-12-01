c9382d3752584af6371ae6188fc758e2dcbbe318
dev-jeong <need9306@gmail.com>
2022-12-01 21:17:49 +0900

    bug fix

--- main.js
+++ main.js
@@ -1,71 +1,89 @@
+'use strict';
+
 require('dotenv').config();
 const puppeteer = require('puppeteer');
 
-let browser, page1, page2, page3, page4, page5
-
-(async () => {
-    browser = await puppeteer.launch({ headless: process.env.HIDE_BROWSER === '0' ? false : true })
-
-    await login()
-
-    while(true){
-        process.env.ACTION1 === '1' ? await soldout() : null
-        process.env.ACTION2 === '1' ? await reinventory() : null
-        process.env.ACTION3 === '1' ? await update() : null
-        process.env.ACTION4 === '1' ? await send() : null
-    }
-
-})();
+let browser;
+let page1;
+let page2;
+let page3;
+let page4;
+let page5;
 
 async function login() {
-    page1 = await browser.newPage()
-    await page1.goto('https://domeggook.com/ssl/member/mem_loginForm.php')
-    await page1.type('#idInput', process.env.ID)
-    await page1.type('#formLogin > input:nth-child(6)', process.env.PW)
-    await page1.click('#formLogin > input.formSubmit')
+    page1 = await browser.newPage();
+    page1.on('console', (msg) => console.log('PAGE1 LOG:', msg.text()));
+
+    await page1.goto('https://domeggook.com/ssl/member/mem_loginForm.php');
+    await page1.type('#idInput', process.env.ID);
+    await page1.type('#formLogin > input:nth-child(6)', process.env.PW);
+    await page1.click('#formLogin > input.formSubmit');
     await page1.waitForNavigation();
-    await page1.goto('https://domemedb.domeggook.com')
+    await page1.goto('https://domemedb.domeggook.com');
 }
 
 async function soldout() {
-    if (page2 === undefined) { page2 = await browser.newPage() }
+    if (page2 === undefined) {
+        page2 = await browser.newPage();
+        page2.on('console', (msg) => console.log('PAGE2 LOG:', msg.text()));
+    }
     await page2.setDefaultNavigationTimeout(0);
-    await page2.goto('https://domemedb.domeggook.com/index/popup_sender/popup_soldOut.php?b2bStatus=0&sstore=0')
-    await page2.evaluate(() => {
-        sendSoldout()
-    });
-    await new Promise(resolve => setTimeout(resolve, parseInt(process.env.INTERVAL) * 1000));
+    await page2.goto('https://domemedb.domeggook.com/index/popup_sender/popup_soldOut.php?b2bStatus=0&sstore=0');
+    await page2.evaluate('sendSoldout()');
+    setTimeout(() => { soldout(); }, parseInt(process.env.INTERVAL, 10) * 1000);
 }
 
 async function reinventory() {
-    if (page3 === undefined) { page3 = await browser.newPage() }
+    if (page3 === undefined) {
+        page3 = await browser.newPage();
+        page3.on('console', (msg) => console.log('PAGE3 LOG:', msg.text()));
+    }
     await page3.setDefaultNavigationTimeout(0);
-    await page3.goto('https://domemedb.domeggook.com/index/popup_sender/popup_reInventory.php?b2bStatus=0&sstore=0')
-    await page3.evaluate(() => {
-        sendReinventory()
-    });
-    await new Promise(resolve => setTimeout(resolve, parseInt(process.env.INTERVAL) * 1000));
+    await page3.goto('https://domemedb.domeggook.com/index/popup_sender/popup_reInventory.php?b2bStatus=0&sstore=0');
+    await page3.evaluate('sendReinventory()');
+    setTimeout(() => { reinventory(); }, parseInt(process.env.INTERVAL, 10) * 1000);
 }
 
 async function update() {
-    if (page4 === undefined) { page4 = await browser.newPage() }
+    if (page4 === undefined) {
+        page4 = await browser.newPage();
+        page4.on('dialog', async dialog => { await dialog.accept(); });
+        page4.on('console', (msg) => console.log('PAGE4 LOG:', msg.text()));
+    }
     await page4.setDefaultNavigationTimeout(0);
-    await page4.goto('https://domemedb.domeggook.com/index/popup_sender/popup_updateProduct.php?arr=all&b2bStatus=0&status=PRICE&fromOversea=0&sf=&sw=&itemNos=&pageLimit=50&b2bStatus=0&template=&sender_date1=&sender_date2=&dateType=3&template=&sstore=0&update=ok&&nmChkVal=1')
-    await new Promise(resolve => setTimeout(resolve, parseInt(process.env.INTERVAL) * 1000));
+    await page4.goto(
+        `https://domemedb.domeggook.com/index/popup_sender/popup_updateProduct.php?
+         update=ok&arr=priceall&b2bStatus=0&nmChkVal=0`
+    );
+    await page4.evaluate('delDataError()');
+    setTimeout(() => { update(); }, parseInt(process.env.INTERVAL, 10) * 1000);
 }
 
 async function send() {
-    if (page5 === undefined) { page5 = await browser.newPage() }
-    page5.on('dialog', async dialog => {
-        await dialog.accept();
-    });
-    await page5.setDefaultNavigationTimeout(0);
-    await page5.goto('https://domemedb.domeggook.com/index/popup_sender/popup_updateProduct.php?arr=all&b2bStatus=0&status=SUCCESS&fromOversea=0&sf=&sw=&itemNos=&pageLimit=50&b2bStatus=0&template=&sender_date1=&sender_date2=&dateType=7&template=&sstore=0&update=ok&&nmChkVal=0')
-
-    await page5.goto('https://domemedb.domeggook.com/index/popup_sender/popup_updateProduct.php?error=ok&so=0')
-    const text = await page5.$eval('#bigDiv2 > div > div.col-md-12 > table > tbody > tr > td:nth-child(4) > div:nth-child(1)', element => element.textContent)
-    if (text.replace(/ /gi, '') != '0건\n') {
-        await page5.click('#bigDiv2 > div > div.col-md-12 > table > tbody > tr > td:nth-child(4) > div.fr > button')
+    if (page5 === undefined) {
+        page5 = await browser.newPage();
+        page5.on('dialog', async dialog => { await dialog.accept(); });
+        page5.on('console', (msg) => console.log('PAGE5 LOG:', msg.text()));
     }
-    await new Promise(resolve => setTimeout(resolve, parseInt(process.env.INTERVAL) * 1000));
+    await page5.setDefaultNavigationTimeout(0);
+    await page5.goto(
+        `https://domemedb.domeggook.com/index/popup_sender/popup_updateProduct.php?
+        arr=all&b2bStatus=0&status=SUCCESS&fromOversea=0&sf=&sw=&itemNos=&pageLimit=50&b2bStatus=0&
+        template=&sender_date1=&sender_date2=&dateType=7&template=&sstore=0&update=ok&&nmChkVal=0`
+    );
+    await page5.evaluate('delDataError()');
+    setTimeout(() => { send(); }, parseInt(process.env.INTERVAL, 10) * 1000);
 }
+
+(async () => {
+    browser = await puppeteer.launch({ headless: process.env.HIDE_BROWSER !== '0' });
+
+    await login();
+
+    Promise.all([
+        process.env.ACTION1 === '1' ? soldout() : null,
+        process.env.ACTION2 === '1' ? reinventory() : null,
+        process.env.ACTION3 === '1' ? update() : null,
+        process.env.ACTION4 === '1' ? send() : null,
+    ]);
+})();
